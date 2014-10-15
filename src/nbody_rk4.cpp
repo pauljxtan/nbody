e8574e536a7bd49f80974d9b45fcb2ddf9a256db
Paul Tan <pjxtan@gmail.com>
2014-10-14 21:37:53 -0400

    Modified constructor for new variables; added parameters for step(); defined step_err() for computing local truncation error; majorly modified integrate() to use adaptive step sizes

--- src/nbody_rk4.cpp
+++ src/nbody_rk4.cpp
@@ -8,19 +8,21 @@
 // TODO:
 //     adaptive step sizes
 
-NBodyRK4::NBodyRK4(int n_bodies, int n_dims, double m[], double dt_init,
-                   double t_init, double X_init[], bool write,
-                   FILE *p_outfile) {
+NBodyRK4::NBodyRK4(int n_bodies, int n_dims, double m[], double t_init,
+                   double X_init[], bool write, FILE *p_outfile) {
     this->n_bodies = n_bodies;
     this->n_dims = n_dims;
     this->m = (double *) malloc(n_bodies * sizeof(double));
     for (int i = 0; i < n_bodies; i++)
         this->m[i] = m[i];
-    this->dt = dt_init;
     this->t = t_init;
     this->X.resize(2 * n_dims * n_bodies);
     for (int i = 0; i < 2 * n_dims * n_bodies; i++)
         this->X[i] = X_init[i];
+    this->X_dot.resize(2 * n_dims * n_bodies, 0.0);
+    this->X_new_1a.resize(2 * n_dims * n_bodies);
+    this->X_new_1b.resize(2 * n_dims * n_bodies);
+    this->X_new_2.resize(2 * n_dims * n_bodies);
     this->write = write;
     this->p_outfile = p_outfile;
 
@@ -30,45 +32,76 @@ NBodyRK4::NBodyRK4(int n_bodies, int n_dims, double m[], double dt_init,
     this->k2.resize(2 * n_dims * n_bodies);
     this->k3.resize(2 * n_dims * n_bodies);
     this->k4.resize(2 * n_dims * n_bodies);
-    this->x_dot.resize(2 * n_dims * n_bodies, 0.0);
 
     print_state();
 }
-std::valarray<double> NBodyRK4::newton_grav(std::valarray<double> x) {
-    //std::valarray<double> x_dot(0.0, 2 * n_dims * n_bodies);
-    // set all values to zero
-    x_dot -= x_dot;
+std::valarray<double> NBodyRK4::newton_grav(std::valarray<double> X) {
+    // Reset all values to zero
+    X_dot -= X_dot;
 
     for (int i = 0; i < n_bodies; i++) {
-        ri = pos(x, i);
-        x_dot[pos_sel(i)] = vel(x, i);
+        ri = pos(X, i);
+        X_dot[pos_sel(i)] = vel(X, i);
 
         for (int j = 0; j < n_bodies; j++) {
             if (j != i) {
-                rj = pos(x, j);
-                x_dot[vel_sel(i)] += G * m[j] * (rj - ri)
+                rj = pos(X, j);
+                X_dot[vel_sel(i)] += G * m[j] * (rj - ri)
                                      / pow(dist(rj, ri), 3);
             }
         }
     }
-    return x_dot;
+    return X_dot;
 }
 
-void NBodyRK4::step() {
+std::valarray<double> NBodyRK4::step(double dt, std::valarray<double> X) {
     k1 = dt * newton_grav(X);
     k2 = dt * newton_grav(X + k1 / 2.0);
     k3 = dt * newton_grav(X + k2 / 2.0);
     k4 = dt * newton_grav(X + k3);
     
-    t += dt;
-    X += (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0;
-
-    print_state();
-    if (write)
-        write_state();
+    return X + (k1 + 2.0 * k2 + 2.0 * k3 + k4) / 6.0;
 }
 
-void NBodyRK4::integrate(int n_steps) {
-    while (n_steps--)
-        step();
+double NBodyRK4::step_err(std::valarray<double> X1, std::valarray<double> X2) {
+    eps_sum_sq = 0.0;
+
+    for (int i = 0; i < n_bodies; i++) {
+        for (int j = 0; j < n_dims; j++)
+            // Get epsilons for position only (not velocity)
+            eps_sum_sq += std::pow((pos(X1, i)[j] - pos(X2, i)[j]) / 30.0, 2);
+    }
+
+    return std::sqrt(eps_sum_sq);
+}
+
+void NBodyRK4::integrate(double dur, double dt_init, double delta) {
+    double dt = dt_init;
+    double t_start = t;
+
+    while (t < t_start + dur) {
+        // First estimate
+        X_new_1a = step(dt, X);
+        X_new_1b = step(dt, X_new_1a);
+        // Second estimate
+        X_new_2 = step(2.0 * dt, X);
+
+        rho = delta * dt / step_err(X_new_1b, X_new_2);
+        if (rho > 1.0) {
+            // Keep step, adjust step size, move on to next iteration
+            t += dt;
+            X = X_new_1a;
+            dt *= std::pow(rho, 0.25);
+        }
+        else {
+            // Adjust step size, redo step, then move on to next iteration
+            dt *= std::pow(rho, 0.25);
+            t += dt;
+            X = step(dt, X);
+        }
+
+        print_state();
+        if (write)
+            write_state();
+    }
 }

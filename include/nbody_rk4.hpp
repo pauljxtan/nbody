8a478371dff2d1b25e8c7970616b1f32fa03ea0f
Paul Tan <pjxtan@gmail.com>
2014-10-14 21:33:49 -0400

    Declared more member variables used to store intermediate data during the adaptive procedure

--- include/nbody_rk4.hpp
+++ include/nbody_rk4.hpp
@@ -5,14 +5,23 @@ class NBodyRK4: public NBody {
     private:
         std::valarray<double> ri;
         std::valarray<double> rj;
+        
         std::valarray<double> k1;
         std::valarray<double> k2;
         std::valarray<double> k3;
         std::valarray<double> k4;
 
+        std::valarray<double> X_new_1a;
+        std::valarray<double> X_new_1b;
+        std::valarray<double> X_new_2;
+
+        double eps_sum_sq;
+        double rho;
+        
     public:
-        NBodyRK4(int, int, double [], double, double, double [], bool, FILE *);
+        NBodyRK4(int, int, double [], double, double [], bool, FILE *);
         std::valarray<double> newton_grav(std::valarray<double>);
-        void step();
-        void integrate(int);
+        std::valarray<double> step(double, std::valarray<double>);
+        double step_err(std::valarray<double>, std::valarray<double>);
+        void integrate(double, double, double);
 };

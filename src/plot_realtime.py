5148b8695e1057d05eb7def4cae578c60b01e1b1
Paul Tan <pjxtan@gmail.com>
2014-11-06 21:55:03 -0500

    Real-time plotting script

---
+++ src/plot_realtime.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import sys
+
+def main():
+    # Set the frameskip (plot every n-th frame)
+    frame_skip = int(sys.argv[1])
+
+    # Enable interactive mode
+    plt.ion()
+
+    # Set up figure
+    fig = plt.figure()
+    sp = fig.add_subplot(111)
+    
+    frame_count = 0
+    while True:
+        line = sys.stdin.readline()
+        
+        # Check if end of simulation
+        if line == '':
+            break
+
+        frame_count += 1
+        # Check if frame should be skipped
+        if frame_count % frame_skip != 0:
+            continue
+
+        # Process data
+        data = map(float, line.split(" "))
+
+        # Update plot
+        sp.set_title("t = %d" % frame_count)
+        sp.scatter(data[1], data[2], s=1, color='b')
+        sp.scatter(data[7], data[8], s=1, color='g')
+        sp.scatter(data[13], data[14], s=1, color='r')
+
+        # Save to file
+        #plt.savefig("./frames/%07d.png" % framecount)
+
+        plt.draw()
+
+if __name__ == "__main__":
+    sys.exit(main())
